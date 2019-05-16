045ddd2b8678dd76bec15e8c7bf4ba2a032c18a9
syahril <syahrilaku17@gmail.com>
2019-05-16 14:33:08 +0700

    CRUD Kelas

---
+++ app/Kelas.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mapel extends Model
+{
+
+    protected $fillable = [
+        'nama_mapel',
+        'id_jurusan'
+    ];
+    protected $table = 'mata_pelajaran';
+
+    public function jurusan()
+    {
+        return $this->belongsTo('App\Jurusan', 'id_jurusan');
+    }
+}
