83890a299781e8ea165ee2285c66dc71e26a83d5
saputrakmdn <saputra.kmdn@gmail.com>
2019-05-16 09:12:27 +0700

    fix

--- resources/views/admin/user/adddetailsiswa.blade.php
+++ resources/views/admin/user/adddetailsiswa.blade.php
@@ -40,7 +40,7 @@
 </div>
 
 @endsection
-@push('titit')
+@push('test')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script>
 $(document).ready(function() {
