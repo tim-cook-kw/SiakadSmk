37bddd992be8b9c93023deb8792b1f49ec800d29
Rizky <rizkyaldi0101@gmail.com>
2019-06-17 22:24:19 +0700

    bug absen

--- resources/views/guru/absensi_siswa/absen.blade.php
+++ resources/views/guru/absensi_siswa/absen.blade.php
@@ -1,19 +1,19 @@
-@extends('guru.guru');
+@extends('guru/guru');
 @section('content1')
 <form action="{{Route('insert.absen')}}" method="post">
 @csrf
 <div class="form-group">
 @foreach($absen as $absn)
-    <label for="nama_jurusan">{{$absn->id}}</label>
-    <input type="text" name="nama_jurusan" class="form-control" id="nama_jurusan">
+    <label for="id_siswa">{{$absn->id}}</label>
+    <input type="text" name="id_siswa" class="form-control" id="id_siswa">
   </div>
   <div class="form-group">
-    <label for="visi">Visi jurusan</label>
-    <input type="text" name="visi" class="form-control" id="visi">
+    <label for="waktu">waktu</label>
+    <input type="text" name="waktu" class="form-control" id="waktu">
   </div>
   <div class="form-group">
-    <label for="misi">Misi Jurusan</label>
-    <input type="text" name="misi" class="form-control" id="misi">
+    <label for="keterangan">keterangan</label>
+    <input type="text" name="keterangan" class="form-control" id="keterangan">
   </div>
   <button type="submit" class="btn btn-default">Submit</button>
 </form>
