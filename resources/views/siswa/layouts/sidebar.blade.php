19e581e6d52f2b23ca9f891f836717b83162e2da
egifrediansyah <panjidwinanto4@gmail.com>
2019-06-27 13:50:02 +0700

    siswa

--- resources/views/siswa/layouts/sidebar.blade.php
+++ resources/views/siswa/layouts/sidebar.blade.php
@@ -27,7 +27,7 @@
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
         <li>
-          <a href="#">
+          <a href="{{ url('murid/absen') }}">
             <span>Absensi</span>
           </a>
         </li>
